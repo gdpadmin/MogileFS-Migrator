4b2f957b08184125dd769bee1b20c6738399d6dd
Roy Inganta Ginting <roy.i.ginting@gdpventure.com>
2013-11-25 15:12:25 +0700

    Remove unneeded information

--- migconfig.py
+++ migconfig.py
@@ -1,6 +1,6 @@
 # Mogilefs configuration
-domain = 'kkcdn'
-clas = 'useravatar'
-trackers = ["172.16.0.74:7001", "172.16.0.75:7001", "172.16.0.76:7001"]
+domain = ''
+clas = ''
+trackers = []
 validation_queue = "validation_job_queue"
 migration_queue = "migration_job_queue"
